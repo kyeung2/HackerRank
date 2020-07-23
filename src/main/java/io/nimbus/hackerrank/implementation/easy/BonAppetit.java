003463d4183127a2f068acc039c13ca097143114
Kye Yeung <yeung.kye@pm.me>
2020-07-23 17:35:46 +0100

    more

---
+++ src/main/java/io/nimbus/hackerrank/implementation/easy/BonAppetit.java
@@ -0,0 +1,22 @@
+package io.nimbus.hackerrank.implementation.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://www.hackerrank.com/challenges/bon-appetit/problem
+ */
+public class BonAppetit {
+
+
+    // k index of idea she didn't eat.
+    static void bonAppetit(List<Integer> bill, int k, int b) {
+
+        List<Integer> itemsSheHad = new ArrayList<>(bill);
+        itemsSheHad.remove(k);
+        int actualCost = itemsSheHad.stream().mapToInt(Integer::intValue).sum() / 2;
+        int owed = b - actualCost;
+
+        System.out.println(owed == 0 ? "Bon Appetit" : owed);
+    }
+}
