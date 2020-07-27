e07902c8a516ad8d03aa62e57011f1c8f147d4bf
Kye Yeung <yeung.kye@pm.me>
2020-07-27 22:15:36 +0100

    more

--- src/main/java/io/nimbus/hackerrank/implementation/easy/AngryProcessor.java
+++ src/main/java/io/nimbus/hackerrank/implementation/easy/AngryProcessor.java
@@ -1,7 +1,15 @@
 package io.nimbus.hackerrank.implementation.easy;
 
+import java.util.Arrays;
+
 /**
  * https://www.hackerrank.com/challenges/angry-professor/problem
  */
 public class AngryProcessor {
+
+    static String angryProfessor(int k, int[] a) {
+
+        long numberOfStudentsOnTime = Arrays.stream(a).filter(time -> time <= 0).count();
+        return numberOfStudentsOnTime >= k ? "NO" : "YES";
+    }
 }
---
+++ src/main/java/io/nimbus/hackerrank/implementation/easy/SaveThePrisoner.java
@@ -0,0 +1,17 @@
+package io.nimbus.hackerrank.implementation.easy;
+
+/**
+ *     static int viralAdvertising(int n) {
+ *
+ *         int cumulative = 0;
+ *         int shared = 5;
+ *         for (int i = 0; i < n; i++) {
+ *             int liked = shared / 2;
+ *             cumulative += liked;
+ *             shared = liked * 3;
+ *         }
+ *         return cumulative;
+ *     }
+ */
+public class SaveThePrisoner {
+}
